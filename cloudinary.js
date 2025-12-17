dadd16b90c6261bb857df36e4e357737c069ba04
vanebev <vanesabevacqua@gmail.com>
2025-12-17 14:08:30 -0300

    Config Cloudinary

--- cloudinary.js
+++ cloudinary.js
@@ -1,3 +1,10 @@
-s98bXBQQwgJsy3SUKJwpj6JIvfM
 
+import { v2 as cloudinary } from 'cloudinary'
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+export default cloudinary
